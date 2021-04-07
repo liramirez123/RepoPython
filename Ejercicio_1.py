57a71361a95de31ec2ee4d61dc7713bb76138801
Luis Ignacio Ramirez Martinez <luisigna27@gmail.com>
2021-04-07 13:48:20 -0500

    Se ha ehecho cambios en Ejercicio_! y Reame

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -9,7 +9,18 @@ class Fraccion:
     
     def __str__(self):
         return str(self.num)+"/"+str(self.den)
+    
+    def __add__(self,otraFraccion):
+        nuevoNum = self.num * otraFraccion.den + self.den * otraFraccion.num
+        nuevoDen = self.den * otraFraccion.den
+
+        return Fraccion(nuevoNum,nuevoDen)
 
 
 miF = Fraccion(1,2)
 miF.mostrar()
+
+f1 = Fraccion(1,4)
+f2 = Fraccion(1,2)
+f3 = f1 + f2
+print(f3)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RepoPython
 Se estará publicando ejercicios en Python
+
+## Ejercicios de  POO Fraccion
+Se está creando el modelado de una fracción
